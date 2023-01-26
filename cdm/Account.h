0f3e9862b24cfb9e255e1ca3d00c771029dbaf7c
Amaury Rodriguez <92987817+amauryrlm@users.noreply.github.com>
2023-01-26 15:41:09 +0100

    Add files via upload

---
+++ cdm/Account.h
@@ -0,0 +1,23 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <string>
+
+class Account
+{
+    public:
+        Account(std::string n, std::string s, std::string e, int a, std::string nat, std::string p): name(n), surname(s), email(e), age(a), nationality(nat), password(p){}
+        Account();
+        virtual ~Account(){}
+
+    protected:
+        std::string name;
+        std::string surname;
+        std::string email;
+        int age;
+        std::string nationality;
+        std::string password;
+};
+
+#endif // ACCOUNT_H
